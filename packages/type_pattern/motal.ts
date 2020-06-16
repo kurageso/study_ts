e8fbaaafbc11913bf7ba55d456f6cdc7388edd72
kurageso <63407106+kurageso@users.noreply.github.com>
2020-06-17 00:49:12 +0900

    型が確定しているので代入するだけ

--- packages/type_pattern/motal.ts
+++ packages/type_pattern/motal.ts
@@ -8,9 +8,11 @@ type Motal<T> = T extends Human ? true : false;
 
 // 問題：　ソクラテスは死ぬか？
 type Q_1 = Motal<"socrates">; // true
+const is_socrates_motal: Motal<"socrates"> = true; // 最初から型が確定しているのでエラーにならないように代入するだけ
 
 // 問題：　hogehogeは死ぬか？
 type Q_2 = Motal<"hogehoge">; // false
+const is_hogehoge_motal: Motal<"hogehoge"> = false; // 最初から型が確定しているのでエラーにならないように代入するだけ
 
 // 静的型付言語での型レベルでのプログラミングなので実行する前から答えがわかっている
 
